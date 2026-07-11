4a63ab7a4c42f92ff8abbfe8289c434a95bc9894
Manish1678-sos <manishbhowmik1234@gmail.com>
2026-07-11 11:02:47 +0530

    Updated Navbar

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './Components/Navbar';
 import News from './Components/News';
+import
+{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+
+}from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar/>
-        <News pageSize={5} country="us" category="sports"/>
+        <Router>
+          <Navbar/>
+          <Routes>
+            
+            <Route exact path="/business" element={<News key="business" pageSize={5} country="us" category="business"/>} />
+            <Route exact path="/entertainment" element={<News key="entertainment" pageSize={5} country="us" category="entertainment"/>} />
+            <Route exact path="/general" element={<News key="general" pageSize={5} country="us" category="general"/>} />
+            <Route exact path="/health" element={<News key="health" pageSize={5} country="us" category="health"/>} />
+            <Route exact path="/science" element={<News key="science" pageSize={5} country="us" category="science"/>} />
+            <Route exact path="/sports" element={<News pageSize={5} country="us" category="sports"/>} />
+            <Route exact path="/technology" element={<News pageSize={5} country="us" category="technology"/>} />
+          </Routes>
+        </Router>
       </div>
     )
   }
